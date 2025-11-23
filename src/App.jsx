8aae7df5c7e35bc33dac35f0e27c86710767b513
Jorge Llanes <lq21001@ues.edu.sv>
2025-11-23 00:12:38 -0600

    Se agrego la base para el  registro de cliente, cuenta personal

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout.jsx";
 import Home from "./pages/Home.jsx";
+import OnboardingWizard from "./components/OnbordingWizard.jsx";
 
 function App() {
   return (
     <MainLayout>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/onboarding" element={<OnboardingWizard />} />
         {/* aquí luego agregaremos más rutas:
             <Route path="/negocios" element={<Negocios />} />
+             
             <Route path="/negocios/:id" element={<DetalleNegocio />} />
         */}
       </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import "../styles/home.css";
 import SearchBar from "../components/SearchBar";
+import OnboardingWizard from "../components/OnbordingWizard";
 
 export default function Home() {
   return (
     <div className="home">
-      <h1 className="home-title">Buscar negocios</h1>
-      <SearchBar />
+      <SearchBar/>
+      
+      
     </div>
   );
 }
